9e52e99ccf62254fc3b7b58c9e9b9e46b31bfdec
ccmnewmarkj <jennifer.cheng@journalism.cuny.edu>
2023-04-11 13:05:54 -0400

    Edit intro text

--- first/config.js
+++ first/config.js
@@ -23,9 +23,9 @@ var config = {
       title: null,
       image: null,
       description:
-        "<a href='https://www.journalism.cuny.edu/centers/center-community-media/' target='_blank'><img src='./images/ccm-logo.png' style='max-width:200px; margin-left: 0 !important;' alt='Center for Community Media logo' /></a><hr class='hr-headline' /><span style='font-size: 0.9em; text-transform: uppercase; color: #15595a;'>Small Business Reporting Fellowship</span><p style='font-weight: 300; font-size: 1.8em;'>First Stories</p><br />In February 2023, the Small Business Reporting Fellowship kicked off with a cohort of journalists from across NYC and the metro area covering the stories of businesses and entrepreneurs in their communities. They cover the ongoing struggles and challenges, whether brought on or worsened by the pandemic, and also the lessons learned, the new opportunities and the successes.<br /><br />Take a tour through the NYC area of the small businesses and owners featured in the first round of the fellowship stories. The location given may be of just one of several businesses profiled in the fellow's story.<br /><br /><span class='note'><a href='https://airtable.com/shrVkHX3gxbhJIoVE/tblFqhLiryOvHz2qb' target='_blank'>Click here</a> to see a list of the stories in the order of this map.</span><hr class='hr-between' /><h4 style='font-family: Roboto Condensed; font-weight: 400; text-transform: uppercase; color: #15595a;'>Scroll down to begin ↓</h4>",
+        "<a href='https://www.journalism.cuny.edu/centers/center-community-media/' target='_blank'><img src='./images/ccm-logo.png' style='max-width:200px; margin-left: 0 !important;' alt='Center for Community Media logo' /></a><hr class='hr-headline' /><span style='font-size: 0.9em; text-transform: uppercase; color: #15595a;'>Small Business Reporting Fellowship</span><p style='font-weight: 300; font-size: 1.8em;'>First Stories</p><br />In February 2023, the Small Business Reporting Fellowship kicked off with a cohort of journalists from across NYC and the metro area covering the stories of businesses and entrepreneurs in their communities.<br /><br />Take a tour through the NYC area of the small businesses and owners featured in the first round of the fellowship stories. The location given may be of just one of several businesses profiled in the fellow's story.<br /><br /><p style='font-family: Roboto Condensed; font-weight: 700; text-transform: uppercase; color: #15595a; font-size: 1em;'>Scroll down to begin ↓</p><hr class='hr-between' /><span class='note'><a href='https://airtable.com/shrVkHX3gxbhJIoVE/tblFqhLiryOvHz2qb' target='_blank'>Click here</a> to see a list of the stories in the order of this map.</span>",
       location: {
-        center: [-74.02, 40.71],
+        center: [-74.05, 40.71],
         zoom: 9.5, //11.25,
         pitch: 0, // "Angle of the map view. 0 is straight down, and 60 is highly tilted.""
         bearing: 0, // "Degrees of rotation clockwise from North (0). Negative values represent counter-clockwise rotation."
@@ -48,7 +48,7 @@ var config = {
       ],
     },
     {
-      // rillera
+      // 1
       id: "rillera",
       alignment: "right",
       hidden: false,
@@ -73,7 +73,7 @@ var config = {
       onChapterExit: [],
     },
     {
-      // pastor
+      // 2
       id: "pastor",
       alignment: "right",
       hidden: false,
@@ -97,7 +97,7 @@ var config = {
       onChapterExit: [],
     },
     {
-      // rajan
+      // 3
       id: "rajan",
       alignment: "right",
       hidden: false,
@@ -122,7 +122,7 @@ var config = {
       onChapterExit: [],
     },
     {
-      // vereau
+      // 4
       id: "vereau",
       alignment: "right",
       hidden: false,
@@ -147,7 +147,7 @@ var config = {
       onChapterExit: [],
     },
     {
-      // chowdhury
+      // 5
       id: "chowdhury",
       alignment: "right",
       hidden: false,
@@ -174,7 +174,7 @@ var config = {
     },
 
     {
-      // panthi
+      // 6
       id: "panthi",
       alignment: "right",
       hidden: false,
@@ -198,7 +198,7 @@ var config = {
       onChapterExit: [],
     },
     {
-      // karpan
+      // 7
       id: "karpan",
       alignment: "right",
       hidden: false,
@@ -222,7 +222,7 @@ var config = {
       onChapterExit: [],
     },
     {
-      // bradley
+      // 8
       id: "bradley",
       alignment: "right",
       hidden: false,
@@ -246,7 +246,7 @@ var config = {
       onChapterExit: [],
     },
     {
-      // edwards
+      // 9
       id: "edwards",
       alignment: "right",
       hidden: false,
@@ -271,7 +271,7 @@ var config = {
       onChapterExit: [],
     },
     {
-      // nguyen
+      // 10
       id: "nguyen",
       alignment: "right",
       hidden: false,
@@ -297,7 +297,7 @@ var config = {
     },
 
     {
-      // lagunova
+      // 11
       id: "lagunova",
       alignment: "right",
       hidden: false,
@@ -322,7 +322,7 @@ var config = {
     },
 
     {
-      // chopra
+      // 12
       id: "chopra",
       alignment: "right",
       hidden: false,
@@ -348,7 +348,7 @@ var config = {
     },
 
     {
-      // phillip
+      // 13
       id: "phillip",
       alignment: "right",
       hidden: false,
@@ -373,6 +373,7 @@ var config = {
       onChapterExit: [],
     },
     {
+      // 14
       id: "end",
       alignment: "left",
       hidden: false,
